937585829beb7846e4d4706c9840d56d69c9b6b5
Shubham Rathod <120051703+shubhamsrathod86@users.noreply.github.com>
2025-11-19 00:18:36 +0530

    Update api.js

--- Client/NotesApp/src/api/api.js
+++ Client/NotesApp/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'https://notes-app-45so.onrender.com',
 });
 
 
